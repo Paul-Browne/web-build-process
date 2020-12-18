ebfa4da4eeef6cd29f808e28ef18408fbec85f7a
paul browne <paulmatthewbrowne@gmail.com>
2020-12-18 13:15:55 +0200

    adds simple <include> method

--- scripts/html.js
+++ scripts/html.js
@@ -1,7 +1,40 @@
+const fs = require('fs');
 const utility = require("./utility.js");
 const minify = require('html-minifier').minify;
 
+const sourceDirectoryName = process.env.SOURCE_DIR_NAME || 'src';
+const selfClosingIncludeTagRegex = /<include[^<]*\/>/g;
+const includeSource = /\s(\S*?)=(?:"|')?(.*?)(?:"|'|\s)/g;
+
+function fetchResource(path){
+	var fileContents;
+	try {
+		fileContents = fs.readFileSync(sourceDirectoryName + path, 'utf8');
+	} catch (err) {
+		fileContents = undefined;
+	}
+	return fileContents;
+}
+
+function returnRegexMatch(string, regex){
+	if(string.match(regex)){
+		return string.match(regex)[0];
+	}else{
+		return false;
+	}
+}
+
 module.exports = function(source, outPath){
+	
+	var includeTags = source.match(selfClosingIncludeTagRegex);
+	includeTags.forEach(function(tag){
+		while(z = includeSource.exec(tag)){
+			if(z[1] == "src"){
+				source = source.replace(tag, fetchResource(z[2]) );
+			}
+		}
+	})
+
 	source = minify(source, {
 	    removeAttributeQuotes: false,
 	    collapseWhitespace: true,
