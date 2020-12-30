76ad516f2407389859cf06e232344a39d780fe1f
paul browne <paulmatthewbrowne@gmail.com>
2020-12-30 12:24:36 +0200

    adds

--- scripts/json.js
+++ scripts/json.js
@@ -1,5 +1,7 @@
 const utility = require("./utility.js");
 const jsonminify = require("jsonminify");
+
+
 module.exports = function(source, outPath){
 	utility.writeOut(jsonminify(source), outPath);
 };
