0103ecd28e537a29ef99c531c1d2837b08754632
paul browne <paulbrowne@pauls-MacBook-Pro.local>
2020-12-30 12:17:06 +0200

    "$1"

--- scripts/json.js
+++ scripts/json.js
@@ -1,7 +1,5 @@
 const utility = require("./utility.js");
 const jsonminify = require("jsonminify");
-
 module.exports = function(source, outPath){
 	utility.writeOut(jsonminify(source), outPath);
 };
-
